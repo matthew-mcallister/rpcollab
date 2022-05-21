0f79946d8f92978c81841f3a67e44fe431e89c8c
Matthew McAllister <matthew.mcallister.0@gmail.com>
2022-05-20 23:29:10 -0700

    Add camera controls (panning + zoom)

--- src/map/MapCanvas.ts
+++ src/map/MapCanvas.ts
@@ -25,6 +25,85 @@ export class Camera {
   }
 }
 
+class CameraController {
+  public camera: Camera;
+  public dragging: boolean = false;
+
+  constructor() {
+    this.camera = new Camera();
+  }
+
+  zoomFactor(): number {
+    return (2 * window.devicePixelRatio) / this.camera.zoom;
+  }
+
+  public onMouseDown(event: MouseEvent): void {
+    if (event.button === 1 || (event.button == 0 && event.shiftKey)) {
+      this.dragging = true;
+    }
+  }
+
+  public onMouseUp(event: MouseEvent): void {
+    if (event.button === 1 || (event.button == 0 && event.shiftKey)) {
+      this.dragging = false;
+    }
+  }
+
+  public onMouseMove(event: MouseEvent): void {
+    // TODO: Preserve momentum when releasing
+    if (!this.dragging) {
+      return;
+    }
+    // TODO: IDK what the exact formula is but this works on my browser.
+    this.camera.position.x -= event.movementX / this.zoomFactor();
+    this.camera.position.y -= event.movementY / this.zoomFactor();
+  }
+
+  public onWheel(event: WheelEvent): void {
+    // TODO: Probably shouldn't use FP math for this.
+    if (event.deltaY < 0) {
+      this.camera.zoom /= 1.1;
+    } else {
+      this.camera.zoom *= 1.1;
+    }
+  }
+
+  // TODO: This is really awkward; it may be better to have one
+  // top-level canvas event listener that forwards the events to
+  // the appropriate handlers.
+  public attach(canvas: HTMLCanvasElement): void {
+    const controller = this;
+
+    function mousedown(event: MouseEvent) {
+      controller.onMouseDown(event);
+    }
+    function mouseup(event: MouseEvent) {
+      controller.onMouseUp(event);
+    }
+    function mousemove(event: MouseEvent) {
+      controller.onMouseMove(event);
+    }
+    function wheel(event: WheelEvent) {
+      controller.onWheel(event);
+    }
+
+    canvas.addEventListener('mousedown', mousedown);
+    canvas.addEventListener('mouseup', mouseup);
+    canvas.addEventListener('mousemove', mousemove);
+    canvas.addEventListener('wheel', wheel);
+
+    controller.detach = () => {
+      canvas.removeEventListener('mousedown', mousedown);
+      canvas.removeEventListener('mouseup', mouseup);
+      canvas.removeEventListener('mousemove', mousemove);
+      canvas.removeEventListener('wheel', wheel);
+      controller.detach = () => {};
+    };
+  }
+
+  public detach(): void {}
+}
+
 /**
  * Canvas-based controller which draws the map and processes interaction.
  * Should take any state as a parameter so it can be used in React
@@ -34,7 +113,7 @@ export default class MapCanvas {
   private map: MapModel;
   private ctx: CanvasRenderingContext2D | null = null;
 
-  public camera: Camera = new Camera();
+  private cameraController: CameraController = new CameraController();
   private xform: Xform2 = new Xform2();
 
   constructor(map: MapModel) {
@@ -43,10 +122,12 @@ export default class MapCanvas {
 
   public attach(canvas: HTMLCanvasElement): void {
     this.ctx = canvas.getContext('2d');
+    this.cameraController.attach(canvas);
   }
 
   public detach(): void {
     this.ctx = null;
+    this.cameraController.detach();
   }
 
   /**
@@ -107,20 +188,8 @@ export default class MapCanvas {
     }
   }
 
-  public updateCamera(): void {
-    const t = performance.now() / 1000;
-    this.camera.position = new Vector2(
-      700 + 10 * (1 + Math.cos((1.227 * t) / 2)),
-      850 + 10 * (1 + Math.sin((2.865 * t) / 2))
-    );
-    this.camera.zoom = 1 + 0.1 * Math.sin(0.2112 * t);
-    this.camera.angle = (Math.PI / 32) * Math.sin(0.15 * t);
-
-    this.updateXform();
-  }
-
   public updateXform(): void {
-    const camera = this.camera.xform();
+    const camera = this.cameraController.camera.xform();
     const canvas = this.canvasXform();
     this.xform = canvas.mul(camera);
   }
@@ -129,7 +198,7 @@ export default class MapCanvas {
     const canvas = this.ctx.canvas;
     this.ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    this.updateCamera();
+    this.updateXform();
 
     const r = 10;
     const h = 2 * r * Math.sin(Math.PI / 3);
@@ -142,9 +211,6 @@ export default class MapCanvas {
       const y_0 = j % 2 === 1 ? h : h / 2;
       for (let i = 0; i < this.map.height; i++) {
         const cell = this.map.cells[j][i];
-        if (!cell) {
-          console.log('missing cell', j, i);
-        }
 
         const strokeColor = new Color(0, 0, 0);
         const p = new Vector2(x_0 + j * dx, y_0 + i * dy);
