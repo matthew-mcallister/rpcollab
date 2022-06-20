67ca1d4309a607cfb13ed46aa9610b5c28267dd2
Matthew McAllister <matthew.mcallister.0@gmail.com>
2022-06-20 15:03:45 -0700

    Show highlighted cell coords in footer

--- src/map/Footer.tsx
+++ src/map/Footer.tsx
@@ -5,11 +5,20 @@ interface FooterProps {}
 
 export default function Footer(props: FooterProps) {
   const state = useMapContext().map.state;
+  const hl = state.highlightedCell;
   return (
     <div className="Footer">
       <div className="FooterContentContainer">
         <div className="FooterLeft">placeholder text :)</div>
         <div className="FooterRight">
+          {hl && (
+            <>
+              <span>
+                {hl.x + 1}, {hl.y + 1}
+              </span>
+              <span> | </span>
+            </>
+          )}
           <span>fps: {state.fps().toFixed(2)}</span>
         </div>
       </div>
